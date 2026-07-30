65458e7f0215b4ea5ba9ba953000c78cba5b0bd9
unknown <pheydari33@yahoo.com>
2026-07-30 18:34:25 +0330

    feat: workflow configurations

---
+++ workflow/workflows.py
@@ -0,0 +1,11 @@
+WORKFLOWS = [
+    "refund_workflow",
+    "payment_workflow",
+    "shipping_workflow",
+    "tracking_workflow",
+    "technical_workflow",
+    "account_workflow",
+    "ordering_workflow",
+    "crm_workflow",
+    "human_review",
+]
