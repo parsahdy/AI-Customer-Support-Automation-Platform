f80f6212ffff820d7c5703d61d898048ec384665
unknown <pheydari33@yahoo.com>
2026-08-02 22:14:37 +0330

    feat: implement RAG response generator

---
+++ llm/rag_generator.py
@@ -0,0 +1,36 @@
+from knowledge_base.retriever import Retriever
+from .prompt_builder import PromptBuilder
+from .llm_factory import LLMFactory
+from .config import config
+
+
+def generator(question: str) -> str:
+
+    # Retrive similar documents 
+    retriever = Retriever()
+    documents = retriever.search(question)
+
+    # Build prompt
+    builder = PromptBuilder()
+
+    builder.set_system_prompt()
+    builder.set_context(documents)
+    builder.set_question(question)
+
+    prompt = builder.build()
+
+    # Set LLM
+    llm = LLMFactory.create(config["provider"])
+
+    # Response
+    response = llm.generate(prompt)
+
+    return response
+
+
+
+if __name__ == "__main__":
+    question = input("Ask any question: ")
+    generator(question)
+
+    
