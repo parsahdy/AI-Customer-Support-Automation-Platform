a6afff1b6b664ef14f824d3f4fad9d69a1f53ad4
unknown <pheydari33@yahoo.com>
2026-08-02 22:13:15 +0330

    feat: add Prompt Builder for RAG prompt construction

--- llm/prompt_builder.py
+++ llm/prompt_builder.py
@@ -4,20 +4,41 @@ from .prompt_templates import SYSTEM_PROMPT, RAG_TEMPLATE
 class PromptBuilder:
 
     def __init__(self):
-        self.parts = []
+        self.system_prompt = ""
+        self.context = ""
+        self.question = ""
 
 
-    def add_system(self):
-        system_prompt = SYSTEM_PROMPT
-        return system_prompt
+    def set_system_prompt(self):
+        self.system_prompt = SYSTEM_PROMPT
+        return self.system_prompt
 
-    def add_context(self, question: str):
-        pass
 
+    def set_context(self, documents):
+        for doc in documents:
+            self.context += doc["content"]
+            self.context += "\n\n"
+        return self.context
         
 
-    def add_question(self):
-        pass
+    def set_question(self, question: str):
+        self.question = question
+        return self.question
+
 
     def build(self):
-        pass
+
+        if not self.system_prompt:
+            raise ValueError("System prompt has not been set.")
+
+        if not self.context:
+            raise ValueError("Context has not been set.")
+
+        if not self.question:
+            raise ValueError("Question has not been set.")
+        
+        return RAG_TEMPLATE.format(
+            system_prompt = self.system_prompt,
+            context = self.context,
+            question = self.question,
+        )
